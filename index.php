b3621916914d05670c592790c1835c559f7419ad
David223-isaac <isaaccastillo222@gmail.com>
2024-04-09 00:19:40 -0500

    Update

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
+<?php
+    require_once ("sentencias.php");
+?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -14,7 +17,7 @@
 <br>
 <section>
 <div class="forma1">
-<img class="ImgRegistrar" src="https://cdn-icons-png.flaticon.com/512/6877/6877544.png">
+<img class="ImgRegistrar" src="https://cdn-icons-png.flaticon.com/512/6877/6877544.png" alt="img">
 <form id="reg" action="" method="post">
     <h2>Registro de usuarios</h2>
     <br>
@@ -26,12 +29,10 @@
     <br>
     <br>
     <?php
-    if(isset($_POST['boton'])){
-        require_once("sentencias.php");
+    if (isset($_POST['boton'])){
         $obj=new pelicula();
         $obj->insertar();
         echo "<span> Los datos han sido ingresados</span>";
-        header("Location: index.php");
     }
     ?>
 </form>
@@ -39,7 +40,7 @@
 </div>
 <br>
 <div class="eliminaru">
-<img class="ImgRegistrar" src="https://cdn-icons-png.flaticon.com/512/2150/2150535.png">
+<img class="ImgRegistrar" src="https://cdn-icons-png.flaticon.com/512/2150/2150535.png" alt ="img">
 <form id="eliminar" action="" method="post">
     <h2>Eliminación de usuarios</h2>
     <br>
@@ -49,8 +50,7 @@
     <input type="submit" name="boton2" value="Eliminar">
     <br>
     <?php
-    if(isset($_POST['boton2'])){
-        require_once("sentencias.php");
+    if (isset($_POST['boton2'])){
         $obj=new pelicula();
         $obj->eliminar();
         echo "<span> Los datos han sido eliminados </span>";
@@ -63,7 +63,7 @@
 </div>
 <br>
 <div class="modificaru">
-<img class="ImgRegistrar" src="https://cdn-icons-png.flaticon.com/512/2808/2808392.png">
+<img class="ImgRegistrar" src="https://cdn-icons-png.flaticon.com/512/2808/2808392.png" alt="img">
 <form id="modificar" action="" method="post">
     <h2>Modificación de usuarios</h2>
     <br>
@@ -74,8 +74,7 @@
     <br>
     <input type="submit" name="boton3" value="Guardar">
     <?php
-    if(isset($_POST['boton3'])){
-        require_once("sentencias.php");
+    if (isset($_POST['boton3'])){
         $obj=new pelicula();
         $obj->modificar();
         echo "<span> Los datos han sido modificados </span>";
@@ -89,23 +88,23 @@
 </section>
 <br>
 <div class="base">
-<img class="ImgRegistrar" src="https://cdn-icons-png.flaticon.com/512/9977/9977386.png">
+<img class="ImgRegistrar" src="https://cdn-icons-png.flaticon.com/512/9977/9977386.png" alt="img">
 <table>
+<caption>Description: This table displays user information</caption>
     <h2>ESTADO ACTUAL DE LA TABLA</h2>
     <th>Id</th>
     <th>Nombre</th>
     <th>Apellidos</th>
     <th>Edad</th>
     <?php
-    require_once("sentencias.php");
     $obj = new pelicula();
     $res = $obj->consultar();
-    while($fila=$res->fetch_assoc()){
+    while ($fila=$res->fetch_assoc()){
         echo "<tr>";
-        echo "<td>".$fila["id"]."</td>";
-        echo "<td>".$fila["nombre"]."</td>";
-        echo "<td>".$fila["apellido"]."</td>";
-        echo "<td>".$fila["edad"]."</td>";
+        echo "<td>".$fila["id"].'</td>';
+        echo "<td>".$fila["nombre"].'</td>';
+        echo "<td>".$fila["apellido"].'</td>';
+        echo "<td>".$fila["edad"].'</td>';
         echo "<tr>";
     }
     ?>
